92c7a337791799eeb127c47a923c9a0ba61391b3
Thomas Levine <_@thomaslevine.com>
2015-05-23 10:43:11 +0000

    make sender work in python3

--- sender.py
+++ sender.py
@@ -9,6 +9,6 @@ socket.connect("tcp://localhost:5555")
 for request in range(10):
     print("Sending request %s" % request)
     time.sleep(1)
-    socket.send(b"%i" % request)
+    socket.send(str(request).encode('ascii'))
     message = socket.recv()
     print("Received reply %s [ %s ]" % (request, message))
