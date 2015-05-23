93f60e2123d5117ed8f0f6560353d99396d93f64
Valentin Haenel <valentin@haenel.co>
2015-05-23 11:23:05 +0200

    adding base listener and sender

---
+++ listener.py
@@ -0,0 +1,24 @@
+import os.path
+
+import bcolz
+import numpy as np
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+if not os.path.exists('db'):
+    print("db not found, creating")
+    ct = bcolz.ctable([np.empty(0, dtype="i8,")],
+                      names=['data'],
+                      rootdir='db')
+else:
+    print("db found, initializing")
+    ct = bcolz.open('db')
+
+while True:
+    message = socket.recv()
+    print("Received request: %s" % message)
+    ct.append((message,))
+    socket.send(b"OK")
---
+++ sender.py
@@ -0,0 +1,14 @@
+import zmq
+import time
+
+
+context = zmq.Context()
+
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")
+for request in range(10):
+    print("Sending request %s" % request)
+    time.sleep(2)
+    socket.send(b"%i" % request)
+    message = socket.recv()
+    print("Received reply %s [ %s ]" % (request, message))
